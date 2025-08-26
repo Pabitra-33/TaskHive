45ea55f9daeee6637ac71845c0c88f7ee3b281c4
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-26 15:24:13 +0530

    created a task entity class to store the tasks data in the database

--- taskmanagerapp/src/main/java/taskmangerapplication/entity/TaskEntity.java
+++ taskmanagerapp/src/main/java/taskmangerapplication/entity/TaskEntity.java
@@ -1,5 +1,44 @@
 package taskmangerapplication.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
 public class TaskEntity {
 
+	@Id
+	private String taskname;
+	private String timetocompletetask;
+	
+	@ManyToOne
+	private UserEntity userEntity; 
+	//our requirement is one user can have more than one task so we used ManyToOne mapping.
+
+	//getters and setters method
+	public String getTaskname() {
+		return taskname;
+	}
+
+	public void setTaskname(String taskname) {
+		this.taskname = taskname;
+	}
+
+	public String getTimetocompletetask() {
+		return timetocompletetask;
+	}
+
+	public void setTimetocompletetask(String timetocompletetask) {
+		this.timetocompletetask = timetocompletetask;
+	}
+
+	public UserEntity getUserEntity() {
+		return userEntity;
+	}
+
+	public void setUserEntity(UserEntity userEntity) {
+		this.userEntity = userEntity;
+	}
 }
