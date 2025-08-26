d80da89dc4d48c9255e3fd7e418fddb7e5d137da
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-26 15:24:31 +0530

    created a task binding class to bind the task data coming from the fronted

--- taskmanagerapp/src/main/java/taskmangerapplication/binding/Task.java
+++ taskmanagerapp/src/main/java/taskmangerapplication/binding/Task.java
@@ -2,4 +2,20 @@ package taskmangerapplication.binding;
 
 public class Task {
 
+	private String taskname;
+	private String timetocompletetask;
+	
+	//getter and setter methods
+	public String getTaskname() {
+		return taskname;
+	}
+	public void setTaskname(String taskname) {
+		this.taskname = taskname;
+	}
+	public String getTimetocompletetask() {
+		return timetocompletetask;
+	}
+	public void setTimetocompletetask(String timetocompletetask) {
+		this.timetocompletetask = timetocompletetask;
+	}
 }
