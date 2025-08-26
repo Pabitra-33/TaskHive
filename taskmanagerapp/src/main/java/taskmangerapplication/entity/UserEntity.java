64b7b811edd359108549f74eb3baeb65c29a07c0
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-26 15:22:56 +0530

    created a user entity class to store the users data

--- taskmanagerapp/src/main/java/taskmangerapplication/entity/UserEntity.java
+++ taskmanagerapp/src/main/java/taskmangerapplication/entity/UserEntity.java
@@ -1,5 +1,49 @@
 package taskmangerapplication.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Users")
 public class UserEntity {
 
+	@Id
+	private String email;
+	private String name;
+	private String pswd;
+	private String gender;
+	private String phno;
+	
+	//getters and setters method
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPswd() {
+		return pswd;
+	}
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
 }
