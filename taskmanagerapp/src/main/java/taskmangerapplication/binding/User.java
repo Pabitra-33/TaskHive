4a6d883c10163e50a1516b655e38e12b8e417b2b
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-26 15:23:47 +0530

    created a User binding class to bind the user data coming from the fronted and then save it in the database for secure and smooth interaction

--- taskmanagerapp/src/main/java/taskmangerapplication/binding/User.java
+++ taskmanagerapp/src/main/java/taskmangerapplication/binding/User.java
@@ -2,4 +2,41 @@ package taskmangerapplication.binding;
 
 public class User {
 
+	private String email;
+	private String name;
+	private String pswd;
+	private String gender;
+	private String phno;
+	
+	//getter and setter methods
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPswd() {
+		return pswd;
+	}
+	public void setPswd(String pswd) {
+		this.pswd = pswd;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
 }
