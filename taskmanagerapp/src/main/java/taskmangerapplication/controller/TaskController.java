8e7126ebec066142b34ff65c973591055eaa47b3
Pabitra-33 <kumarpabitra345@gmail.com>
2025-08-30 22:49:54 +0530

    created a separate task controller to work and interact works related to task entity

--- taskmanagerapp/src/main/java/taskmangerapplication/controller/TaskController.java
+++ taskmanagerapp/src/main/java/taskmangerapplication/controller/TaskController.java
@@ -1,5 +1,13 @@
 package taskmangerapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import taskmangerapplication.dao.TaskDao;
+
+@Controller
 public class TaskController {
 
+	@Autowired
+	private TaskDao taskDao;
 }
