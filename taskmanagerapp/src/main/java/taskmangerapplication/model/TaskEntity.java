213fd5e2b4ac7b27581a4d44ea64919cb1c020fa
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-01 00:08:06 +0530

    updated some new fields in our task entity class

--- taskmanagerapp/src/main/java/taskmangerapplication/model/TaskEntity.java
+++ taskmanagerapp/src/main/java/taskmangerapplication/model/TaskEntity.java
@@ -1,7 +1,10 @@
 package taskmangerapplication.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -10,20 +13,41 @@ import jakarta.persistence.Table;
 public class TaskEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
 	private String taskname;
+	private String description;
 	private String timetocompletetask;
 	
 	@ManyToOne
-	private UserEntity userEntity; 
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserEntity userEntity; // each task belongs to a user
 	//our requirement is one user can have more than one task so we used ManyToOne mapping.
 
+	
 	//getters and setters method
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getTaskname() {
 		return taskname;
 	}
 
 	public void setTaskname(String taskname) {
 		this.taskname = taskname;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getTimetocompletetask() {
